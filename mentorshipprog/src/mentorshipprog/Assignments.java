a7f92998528d7d36b4932eadb0774f488cf361ac
Lateefah Smellie <lateefah.smellie@gmail.com>
2019-11-27 10:29:37 -0500

    updated thr assignment functionality

--- mentorshipprog/src/mentorshipprog/Assignments.java
+++ mentorshipprog/src/mentorshipprog/Assignments.java
@@ -6,39 +6,61 @@ public class Assignments {
 	
 	 public static ArrayList<Mentor> mentorlst = new ArrayList<>();
 	 public static ArrayList<Mentee> menteelst = new ArrayList<>();
-		public Assignments(Mentee men) {
-			menteelst.add(men);
-			if(mentorlst.size()==0) {
-				System.out.print("No mentees are avaiable rn");
+
+	 //tries to find mentor match for mentee
+	 public Assignments(Mentee men) {
+	 	boolean assignmentMade = false;
+	 	int count = 0;
+
+	 	do while (assignmentMade == false && count < mentorlst.size()) {
+	 		Mentor ment = mentorlst.get(count);
+			boolean pairAvailable = (ment.status == status.Unavailable) && (men.status == menteeStatus.NotAssigned);
+			boolean majorMatch = ment.getMajor().equals(men.getMajor());
+
+			if (pairAvailable && majorMatch) {
+				assign(ment, men);
+				assignmentMade = true;
 			}
-			else {
-			assign(mentorlst.get(mentorlst.size()-1),men);}
-			
+			count++;
 		}
-	public Assignments(Mentor men) {
-		mentorlst.add(men);
-		if(menteelst.size()==0) {
-			System.out.print("No mentees are avaiable rn");
-		}else {
-		assign(men,menteelst.get(menteelst.size()-1));}
-		
-	}
-	public Assignments() {
-		
-	}
-	
-	
-	
-	public void assign(Mentor ment , Mentee men) {
-		if(ment.getMajor().equals(men.getMajor())) {
-			ment.addMentee(men);
-			men.addMentor(ment);
-		}else {
-			System.out.print("No mentee/Mentor available rn");
+
+	 	menteelst.add(men);
+
+	 	if (!assignmentMade) {
+	 		System.out.println("We're sorry but we could not find a mentor for you. We'll notify you when on is available.");
 		}
-	
+	 }
+
+	public Assignments(Mentor ment) {
+		boolean assignmentMade = false;
+		int count = 0;
+
+		do while (assignmentMade == false && count < menteelst.size()) {
+			Mentee men = menteelst.get(count);
+			boolean pairAvailable = (ment.status == status.Unavailable) && (men.status == menteeStatus.NotAssigned);
+			boolean majorMatch = ment.getMajor().equals(men.getMajor());
+
+			if (pairAvailable && majorMatch) {
+				assign(ment, men);
+				assignmentMade = true;
+			}
+			count++;
+		}
+
+		mentorlst.add(ment);
+
+		if (!assignmentMade) {
+			System.out.println("We're sorry but we could not find a mentee for you. We'll notify you when on is available.");
+		}
 		
 	}
+
+	//assigns mentor to mentee record and mnetee to mentor record.
+	public void assign(Mentor ment, Mentee men) {
+	 	ment.addMentee(men);
+	 	men.addMentor(men);
+	}
+
 	
 	public String toString() {
 		String m="";
@@ -48,7 +70,6 @@ public class Assignments {
 		}
 		return m;
 	}
-	
 	
 
 }
